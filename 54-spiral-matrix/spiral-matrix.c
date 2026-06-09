014ab53a5382ff198d14d8ae03c020af30ba71fb
saksham-codes10 <sakshamsingh472@gmail.com>
2026-06-09 12:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (55.23%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.c
+++ 54-spiral-matrix/spiral-matrix.c
@@ -1,3 +1,6 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
     int rows = matrixSize;
     int cols = matrixColSize[0];
@@ -5,33 +8,28 @@ int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSi
     *returnSize = rows * cols;
     int* result = (int*)malloc((*returnSize) * sizeof(int));
 
-    int top = 0, bottom = rows - 1;
-    int left = 0, right = cols - 1;
+    int top = 0;
+    int bottom = rows - 1;
+    int left = 0;
+    int right = cols - 1;
+
     int idx = 0;
 
     while (top <= bottom && left <= right) {
-
-        // Left -> Right
         for (int j = left; j <= right; j++) {
             result[idx++] = matrix[top][j];
         }
         top++;
-
-        // Top -> Bottom
         for (int i = top; i <= bottom; i++) {
             result[idx++] = matrix[i][right];
         }
         right--;
-
-        // Right -> Left
         if (top <= bottom) {
             for (int j = right; j >= left; j--) {
                 result[idx++] = matrix[bottom][j];
             }
             bottom--;
         }
-
-        // Bottom -> Top
         if (left <= right) {
             for (int i = bottom; i >= top; i--) {
                 result[idx++] = matrix[i][left];
